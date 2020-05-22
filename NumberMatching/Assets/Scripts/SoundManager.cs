52da27a30cc21b2397f6d1972cd67055da80497e
cody brunty <codybrunty@gmail.com>
2020-05-22 01:17:34 -0700

    Added Sound Manager and SFX
    4 SFX for gameplay and a soundmanager to play sounds

---
+++ NumberMatching/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour{
+
+    public static SoundManager SM;
+
+    public Sound[] sounds;
+
+    private void Awake() {
+        if (SM == null) {
+            SM = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else {
+            Destroy(gameObject);
+            return;
+        }
+
+
+        foreach (Sound s in sounds) {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    public void PlayOneShotSound(string name) {
+        //int soundOn = FindObjectOfType<SoundButtonMechanics>().soundOn;
+        //if (soundOn == 1) {
+        //}
+
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null) {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        else {
+            s.source.PlayOneShot(s.source.clip);
+        }
+
+    }
+
+}
